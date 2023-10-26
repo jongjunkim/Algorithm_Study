c3d954d6d33c8f3f77814fbff7774618b6b95bc8
jongjunkim <54413901+jongjunkim@users.noreply.github.com>
2023-10-26 18:13:26 -0400

    Delete 0602-friend-requests-ii-who-has-the-most-friends directory

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++
@@ -1,9 +0,0 @@
-# Write your MySQL query statement below
-
-
-
-with base as(select requester_id id from RequestAccepted
-union all
-select accepter_id id from RequestAccepted)
-
-select id, count(*) as num from base group by id order by num desc limit 1
