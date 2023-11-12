3ab778570811f20c73613640d832ca62deb47d96
jongjunkim <54413901+jongjunkim@users.noreply.github.com>
2023-11-12 15:38:00 -0500

    Update 0046-permutations.py

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -15,10 +15,14 @@ class Solution:
             
             for num in nums:
                 if num not in subset:
-                    subset.append(num)
+                    subset.append(num) 
                     dfs(index + 1)
                     subset.pop()
 
         dfs(0)
 
         return res
+
+
+* [1,2,3] 이 있을때 subset에 nums가 3이 맨처음 들어오고 나서 다음 재귀호출할때는 다시 for num in nums:는 1 부터 돌아간다 이미 subset에는 3이 있으므로 1이 들어가고 [3,1]이라는 subset이 만들어진다.
+* 이게 가능한 이유는 index가 len(nums)보다 클때 끝나는게 아니라 index는 여기서 subset의 개수 즉 subset이 len(nums)와 같을때 재귀호출이 끝나기 때문에 
