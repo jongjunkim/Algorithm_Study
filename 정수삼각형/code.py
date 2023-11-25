c33cd97e67962f620dcb207bffafd84103aef74f
Jongjun Kim <54413901+jongjunkim@users.noreply.github.com>
2023-11-24 22:20:21 -0500

    Create code.py

---
+++ 정수삼각형/code.py
@@ -0,0 +1,12 @@
+
+def solution(triangle):
+    n = len(triangle)
+
+    for i in range(n - 2, -1, -1):
+        for j in range(len(triangle[i])):
+            triangle[i][j] += max(triangle[i + 1][j], triangle[i + 1][j + 1])
+
+    return triangle[0][0]
+
+
+https://school.programmers.co.kr/learn/courses/30/lessons/43105#
