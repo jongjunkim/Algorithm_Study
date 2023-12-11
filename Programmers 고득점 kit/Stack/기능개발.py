ac0315a799cd491749a041d414a6460b430417ac
Jongjun Kim <54413901+jongjunkim@users.noreply.github.com>
2023-12-11 18:14:01 -0500

    Update 기능개발.py

--- Programmers 고득점 kit/Stack/기능개발.py
+++ Programmers 고득점 kit/Stack/기능개발.py
@@ -5,7 +5,7 @@ def solution(progresses, speeds):
     pro = [-1]
     count = 0
     for progress, speed in zip(progresses, speeds):
-        temp =  math.ceil((100 - progress) / speed) # // floor division 5//3 = 1. / regular division 5/3 = 1.333
+        temp =  math.ceil((100 - progress) / speed) # // floor division 5//3 = 1. / regular division 5/3 = 1.666
         if pro and pro[-1] < temp:
             stack.append(count)
             count = 0
