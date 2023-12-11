b8ce879e354a9f7481f539e067be633953ef78a4
Jongjun Kim <54413901+jongjunkim@users.noreply.github.com>
2023-12-11 18:13:08 -0500

    Create 기능개발.py

---
+++ Programmers 고득점 kit/Stack/기능개발.py
@@ -0,0 +1,17 @@
+import math
+def solution(progresses, speeds):
+    
+    stack = []
+    pro = [-1]
+    count = 0
+    for progress, speed in zip(progresses, speeds):
+        temp =  math.ceil((100 - progress) / speed)
+        if pro and pro[-1] < temp:
+            stack.append(count)
+            count = 0
+        pro.append(max(pro[-1], temp))
+        count += 1
+    stack.append(count)
+    
+
+    return stack[1:]
