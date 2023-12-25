e5979f76af67716e04a548128e8dcc0994e3b8bd
Jongjun Kim <54413901+jongjunkim@users.noreply.github.com>
2023-12-25 17:30:09 -0500

    Create 최고의 집합.py

---
+++ Progremmers/Level3/최고의 집합.py
@@ -0,0 +1,14 @@
+def solution(n, s):
+    answer = []
+    if s // n <= 0: return [-1]
+    answer = [s // n for _ in range(n)]
+    
+ 
+    if s % n == 0:
+        return sorted(answer)
+    
+    for i in range(s % n):
+        print(i)
+        answer[i] += 1
+        
+    return sorted(answer)
