8ca34ef7078d95e875a2333c49558761c3f0f89a
Jongjun Kim <54413901+jongjunkim@users.noreply.github.com>
2023-12-30 20:13:28 -0500

    Update 캐시.py

--- Progremmers level2/캐시.py
+++ Progremmers level2/캐시.py
@@ -15,15 +15,13 @@ def solution(cacheSize, cities):
         if cache and city in cache:
             db += 1
             cache.remove(city)
-            cache.append(city)
-            continue
         elif city not in cache and len(cache) < cacheSize:
             db += 5
-            cache.append(city)
-        elif city not in cache and len(cache) >= cacheSize:
+        else:
             db += 5
             cache.popleft()
-            cache.append(city)
+        cache.append(city)        
     
     return db
-#LRU 정의를 보면 cache hit를 하면 그대로 캐시를 두는게 아니라 최근에 사용됬으므로 cache맨 뒤에 넣어주는게 맞다. 그래서 city가 cache에 있으면 그 해당 city를 삭제한후 맨 뒤에 넣어줌
+
+    
