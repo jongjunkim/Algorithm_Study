33c8c643f3f1c05e97d817ac09e7e95088ae79a9
Jongjun Kim <54413901+jongjunkim@users.noreply.github.com>
2024-01-05 19:23:31 -0500

    Update N 개의 최소공배수.py

--- Progremmers level2/N 개의 최소공배수.py
+++ Progremmers level2/N 개의 최소공배수.py
@@ -6,12 +6,11 @@ def solution(arr):
     maxi = max(arr)
     total = math.prod(arr)
     
-    for i in range(maxi, total+1):
-        count = 0
-        for j in arr:
-            if i % j == 0:
-                count += 1
-        if count == len(arr):
+    for i in range(max(arr), multi+1):
+        for num in arr:
+            if i % num != 0:
+                break;
+        else:
             return i
 # 최소공배수 최대공약수는 이미 직관적인 코드가 있을거라고 생각해서 그냥 보고했음
 # 최대공약수
