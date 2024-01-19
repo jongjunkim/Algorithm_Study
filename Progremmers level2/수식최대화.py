0f6b5702841aea56f44e55bbd14d49c9cc26b7ec
Jongjun Kim <54413901+jongjunkim@users.noreply.github.com>
2024-01-19 13:25:04 -0500

    Create 수식최대화.py

---
+++ Progremmers level2/수식최대화.py
@@ -0,0 +1,15 @@
+from itertools import permutations
+def solution(expression):
+    symbol = ['+', '-', '*']
+    answer = []
+    
+    print(expression.split("-"))
+    
+    for per in permutations(symbol):
+        f, s = per[0], per[1]
+        lst = []
+        for e in expression.split(f):
+            temp = [f"({i})" for i in e.split(s)]
+            lst.append(f'({s.join(temp)})')
+        answer.append(abs(eval(f.join(lst))))
+    return max(answer)
