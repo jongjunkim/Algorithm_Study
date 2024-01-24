f97cb7a8f3deb3f96acfc38375adf8f592a8d20a
Jongjun Kim <54413901+jongjunkim@users.noreply.github.com>
2024-01-24 13:15:19 -0500

    Update 듀큐합같게만들기.py

--- Progremmers level2/듀큐합같게만들기.py
+++ Progremmers level2/듀큐합같게만들기.py
@@ -36,4 +36,32 @@ def solution(queue1, queue2):
 
 # 맨처음 while q2 and q1_sum < q2_sum -> if  q1_sum < q2_sum 이라고 하니 시간이 더 오래 걸리고 시간초과가 뜬다
 # if count > limit: return -1 경우도 if not q1 or not q2라고 하니까 시간초과가 뜬다.
+
+
+from collections import deque
+
+def solution(queue1, queue2):
+    
+    q1 = deque(queue1)
+    q2 = deque(queue2)
+    
+    if (sum(q1) + sum(q2)) % 2 != 0:
+        return -1
+    
+    target = (sum(q1) + sum(q2)) // 2
+    ans = 0
+    q1_sum = sum(q1)
+    while q1 and q2 and q1_sum != target:
+        
+        if q1_sum < target:
+            num = q2.popleft()
+            q1.append(num)
+            q1_sum += num
+        elif q1_sum > target:
+            num = q1.popleft()
+            q1_sum -= num
+        
+        ans+=1
+    
+    return ans if q1 and q2 else -1
         
