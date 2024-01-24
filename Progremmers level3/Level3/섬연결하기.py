f61294e07e4958bb4eff752a6eb787345757b38f
Jongjun Kim <54413901+jongjunkim@users.noreply.github.com>
2024-01-24 18:40:48 -0500

    Update 섬연결하기.py

--- Progremmers level3/Level3/섬연결하기.py
+++ Progremmers level3/Level3/섬연결하기.py
@@ -1,33 +1,33 @@
 import heapq
+
 def solution(n, costs):
     
-    costs.sort()
-    
-    vertex = {i:[] for i in range(n)}
-    
-    INF = float('inf')
-    
-    for x,y,c in costs:
-        vertex[x].append((y,c))
-        vertex[y].append((x,c))
-    
-    mst_node = [-1 for _ in range(n)]
-    visited = [True for _ in range(n)]
-    
-    q = [(0,0,0)]
-    while q:
-        cost, start, end = heapq.heappop(q)
-        if visited[start] is False:
-            continue
-        visited[start] = False    
-        mst_node[start] = cost
+    limit = n
+    graph = {i:[] for i in range(n)}
+    distance = {i:float("inf") for i in range(n)}
+    for x,y,w in costs:
+        graph[x].append((w,y))
+        graph[y].append((w,x))
         
-        for dst, weight in vertex[start]:
-            if visited[dst] == True:
-                heapq.heappush(q, (weight, dst, start))
-                print(weight, dst, start)
-     
-
-    return sum(mst_node)
-
-#프림스 알고리즘 이용 weight를 heappush하면서 가장 낮은 weight를 일단은 pop하고 만약 그게 이미 visited에 있다면 continue 아니라면 visited에 넣고  most_node에 추가
+    visited = set()
+    print(graph)
+    q = [(0,0)]
+    
+    ans = 0
+    while q:
+        weight, node = heapq.heappop(q)
+        
+        if node in visited:
+            continue
+    
+        visited.add(node)
+        ans += weight
+        
+        if len(visited) == limit:
+            return ans
+        
+        for w,n in graph[node]:
+            if n not in visited:
+                heapq.heappush(q, (w,n))
+        
+# visited.add() 하고 ans+= weight를 해야 if len(visited) == limit return ans
