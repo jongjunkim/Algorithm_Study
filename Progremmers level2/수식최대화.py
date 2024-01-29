18ade51a6c9d546de2b210eb9c3c56fe8fca8d3b
Jongjun Kim <54413901+jongjunkim@users.noreply.github.com>
2024-01-29 12:49:52 -0500

    Update 수식최대화.py

--- Progremmers level2/수식최대화.py
+++ Progremmers level2/수식최대화.py
@@ -13,3 +13,32 @@ def solution(expression):
             lst.append(f'({s.join(temp)})')
         answer.append(abs(eval(f.join(lst))))
     return max(answer)
+
+
+from itertools import permutations
+import re
+def operate(e,a,b):
+    if e == '+':
+        return a+b
+    if e == '-':
+        return a-b
+    if e == '*':
+        return a*b
+def solution(expression):
+    result = []
+    s = []
+    op_lists = list(permutations(['+','-','*'],3))
+    expression = re.split("([^0-9])",expression) #re library (^) means "starts with"
+    for op_list in op_lists:
+        exp = expression
+        for op in op_list:
+            s = []
+            for e in exp:
+                if s and s[-1] == op:
+                    operation = s.pop()
+                    s[-1] = operate(operation,int(s[-1]),int(e))
+                else:
+                    s.append(e)
+            exp = s
+        result.append(abs(exp[0]))
+    return max(result)
