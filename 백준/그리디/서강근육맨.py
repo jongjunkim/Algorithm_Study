2681b1912eb64c3cc30c87ec915528b91679c589
Jongjun Kim <54413901+jongjunkim@users.noreply.github.com>
2024-02-01 13:00:07 -0500

    Create 서강근육맨.py

---
+++ 백준/그리디/서강근육맨.py
@@ -0,0 +1,17 @@
+
+N = int(input())
+
+loss = list(map(int, input().split()))
+
+tmp = []
+
+loss.sort()
+
+if(N%2==1):
+    tmp.append(loss[-1])
+    loss = loss[:-1]
+
+for i in range(len(loss)//2):
+    tmp.append(loss[i] + loss[len(loss)-1-i])
+
+print(max(tmp))    
